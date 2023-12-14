45a61266164170fed820cbdcedf205b7df0f7354
Milutin Vojnović <milutin13@gmail.com>
2023-12-14 20:57:17 +0100

    ideeeee

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Initial data
     const initialData = [
-        { name: 'Aleksandar Vučić - Srbija ne sme da stane', y: 42, color: '#001F3F', label: 'SNS' },
-        { name: 'Ivica Dačić - premijer Srbije', y: 17, color: '#FF0000', label: 'SPS' },
-        { name: 'ПЕ', y: 29, color: '#2196F3', label: 'Лидер 3' },
-        { name: 'Dr Miloš Jovanović - Nada za Srbiju', y: 37, color: '#8E44AD', label: 'NADA' },
-        { name: 'ДС', y: 6, color: '#FF5722', label: 'Лидер 5' },
-        { name: 'ЈС', y: 17, color: '#673AB7', label: 'Лидер 6' },
-        { name: 'Народна', y: 27, color: '#009688', label: 'Лидер 7' },
-        { name: 'НПС—ЕУ—НЛС', y: 10, color: '#FF9800', label: 'Лидер 8' },
-        { name: 'Milica Đurđević Stamenkovski - Boško Obradović - Nacionalno okupljanje - Državotvorna snaga', y: 16, color: '#000000', label: 'Dveri/Zavetnici' },
-        { name: 'СДП', y: 23, color: '#9C27B0', label: 'Лидер 10' },
-        { name: 'ПУПС', y: 12, color: '#607D8B', label: 'Лидер 11' },
-        { name: 'Двери', y: 20, color: '#FFEB3B', label: 'Лидер 12' },
-        { name: 'СПП—ДСХВ—УСС', y: 23, color: '#8BC34A', label: 'Лидер 13' },
-        { name: 'Заједно', y: 8, color: '#03A9F4', label: 'Лидер 14' },
-        { name: 'СВМ', y: 26, color: '#FF5252', label: 'Лидер 15' },
-        { name: 'ЗЛФ', y: 4, color: '#CDDC39', label: 'Лидер 16' },
+        { name: 'Aleksandar Vučić', y: 30, color: '#4CAF50', label: 'SNS' },
+        { name: 'Ivica Dačić', y: 20, color: '#FFC107', label: 'SPS' },
+        { name: 'Dr Vojislav Šešelj', y: 25, color: '#2196F3', label: 'SRS' },
+        { name: 'Milica Đurđević Stamenkovski', y: 15, color: '#E91E63', label: 'Dveri' },
+        { name: 'Dr Miloš Jovanović', y: 30, color: '#FF5722', label: 'NADA' },
+        { name: 'VAJDASÁGI MAGYAR SZÖVETSÉG', y: 10, color: '#9C27B0', label: 'VMSZ' },
+        { name: 'Srbija protiv nasilja', y: 20, color: '#607D8B', label: 'SPN' },
+        { name: 'Usame Zukorlić', y: 5, color: '#FF9800', label: 'UzP' },
+        { name: 'SDA SANDžAKA', y: 15, color: '#795548', label: 'SDA' },
+        { name: 'Zajedno za budućnost i razvoj', y: 10, color: '#FFEB3B', label: 'ZZBiR' },
+        { name: 'Narodna stranka', y: 10, color: '#9E9E9E', label: 'NS' },
+        { name: 'Saša Radulović - Boris Tadić', y: 15, color: '#673AB7', label: 'DJB-SDS' },
+        { name: 'Politička borba Albanaca', y: 5, color: '#8BC34A', label: 'PBA' },
+        { name: 'Mi - glas naroda', y: 5, color: '#3F51B5', label: 'MG' },
+        { name: 'Srbija na zapadu', y: 10, color: '#FF5252', label: 'SNZ' },
+        { name: 'Ruska stranka', y: 5, color: '#00BCD4', label: 'RS' },
+        { name: 'Čedomir Jovanović', y: 5, color: '#FF1744', label: 'CJ' },
+        { name: 'Albanska demokratska alternativa', y: 5, color: '#009688', label: 'ADA' },
     ];
+
+    const slidersContainer = document.createElement('div');
+    document.body.appendChild(slidersContainer);    
 
     // Create the chart
     const chart = Highcharts.chart('container', {
@@ -25,6 +30,15 @@ document.addEventListener('DOMContentLoaded', function () {
             type: 'item',
             height: '1000px',
         },
+        
+        title: {
+            text: 'Parlamentarni izbori',
+            style: {
+                fontSize: '24px',
+                fontWeight: 'bold',
+            },
+        },
+
         series: [{
             name: 'Representatives',
             keys: ['name', 'y', 'color', 'label'],
@@ -38,8 +52,8 @@ document.addEventListener('DOMContentLoaded', function () {
             },
             center: ['50%', '50%'],
             size: '80%',
-            startAngle: -100,
-            endAngle: 100
+            startAngle: -90,
+            endAngle: 90
         }],
     });
 
@@ -50,14 +64,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to create sliders
     function createSlider(item, index) {
-        const container = document.createElement('div');
-        container.innerHTML = `
+        // Create a container for each slider
+        const sliderContainer = document.createElement('div');
+        sliderContainer.className = 'slider-container'; // You can add a class for styling if needed
+        sliderContainer.innerHTML = `
             <label for="slider${index}">${item.name} (${item.label}): </label>
             <input type="range" id="slider${index}" min="0" max="100" step="1" value="${item.y}">
             <span id="sliderValue${index}">${item.y}</span>
         `;
-        document.body.appendChild(container);
-
+        slidersContainer.appendChild(sliderContainer);
         const slider = document.getElementById(`slider${index}`);
         const sliderValue = document.getElementById(`sliderValue${index}`);
 
